939c7cf769c3589909c546a639f22dadcb70aa6d
Andres <100623904+Patatatat@users.noreply.github.com>
2025-11-13 14:21:53 -0700

    test de la base de datos error 404

--- php/proceso.php
+++ php/proceso.php
@@ -5,13 +5,14 @@ $nomMedico = $_POST['medico'];
 $fecha = $_POST['fecha'];
 $hora = $_POST['hora']; #Variables del index.html
 $especialidad = $_POST['profecion'];
+echo $nomPaciente, "<br>";
 
 //conexion en base de datos, formulario html y proceso
 if ($nomPaciente =="" || $nomMedico == "" || $fecha == "" || $hora == "" || $especialidad == "") {
 	echo "Faltan llenar campos";
 }
 else{
-	$sql = 'INSERT INTO usuario(Nombre_Paciente, Nombre_Medico, Fecha, Hora, Especialidad_Medico) VALUES ("'.$nomPaciente.'","'.$nomMedico.'","'.$fecha.'","'.$hora.'","'.$especialidad.'")';
+	$sql = 'INSERT INTO registro(Nombre_Paciente, Nombre_Medico, Fecha, Hora, Especialidad_Medico) VALUES ("'.$nomPaciente.'","'.$nomMedico.'","'.$fecha.'","'.$hora.'","'.$especialidad.'")';
 
 	//Ejecutar SQL de insercion a la BD
 	$resultado = DataBase::ejecutar_sql($sql);
